575efd021c274c98f669ef5cb29874ae8d8f091e
kennWNK97 <castro21.kc@gmail.com>
2020-12-17 22:05:08 -0600

    Codigo segunda clase

--- Clase2/app.js
+++ Clase2/app.js
@@ -1,8 +1,16 @@
 const http = require('http');
+//const hola = require('../Codigo_Tercero/app2');
+
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "application/json" })
-    res.write('')
+    // res.writeHead(200, { "Content-Type": "application/json" })
+    res.write('Hola KENN Bonita')
     res.end();
 })
 
+//const server = http.createServer(callback)
+
+
+//function callback(req, res) {
+
+//}
 server.listen(8080);
